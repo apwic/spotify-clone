e42c24cab6b443b8ef1af6d5eec65eb100ccca02
sarahzka <13520083@std.stei.itb.ac.id>
2022-10-23 13:15:21 +0700

    feat: query and global styles

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="styles/global.css" />
-        <link rel="stylesheet" href="styles/styles.css" />
-        <title>Test</title>
-    </head>
-    <body>
-        <div class="test">ANCA KENA SANDWICH AZKA</div>
-    </body>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Sepotipayi</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+    <link rel="stylesheet" href="styles/global.css" />
+    <link rel="stylesheet" href="styles/styles.css" />
+</head>
+
+<body>
+    <div class="test">ANCA KENA SANDWICH AZKA</div>
+</body>
+
 </html>
