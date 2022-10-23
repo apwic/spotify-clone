e4646f5483e14760265dec45da9065f2867a79fd
azkazkazka <sarah_azka@yahoo.com>
2022-10-23 11:36:52 +0700

    feat: global styles

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles/global.css" />
+        <link rel="stylesheet" href="styles/styles.css" />
+        <title>Test</title>
+    </head>
+    <body>
+        <div class="test">ANCA KENA SANDWICH AZKA</div>
+    </body>
+</html>
