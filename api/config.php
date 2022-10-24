3ffb148e9d0380fff2637ae60fd1a23128b1000a
irsyadazka <16520096@mahasiswa.itb.ac.id>
2022-10-24 21:54:21 +0700

    feat: config for auth

--- api/config.php
+++ api/config.php
@@ -1,14 +1,16 @@
 <?php
+
+// set header content type
 header('Content-Type: application/json');
 
-//SET CONNECTION
+// set database connection
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
 define('DBNAME', 'sepotipayi');
 
-//SET CONSTANT
-define('PAGE_SIZE', 10);
+// set constant
+define('PAGESIZE', 10);
 
 $con = new mysqli(HOST, USER, PASS, DBNAME);
 
@@ -16,4 +18,60 @@ if ($con->connect_errno) {
   $result = ["status" => "error", "description" => "cannot connect to database"];
   http_response_code(401);
   exit(json_encode($result));
+}
+
+// check is user already logged in
+function isUserAlreadyLoggedIn($con) {
+  // getting token from session
+  $headers = getallheaders();
+
+  if (!isset($headers['sessionToken'])) {
+    return -1;
+  }
+
+  $sessionToken = $headers['sessionToken'];
+
+  // check session in database
+  $sqlQuery = $con->prepare('SELECT * FROM `SESSION` WHERE `session_id` = ?');
+  $sqlQuery->bind_param('s', $sessionToken);
+  $sqlQuery->execute();
+  $queryResult = $sqlQuery->get_result();
+
+  // no data
+  if ($queryResult->num_rows == 0) {
+    return -1;
+  }
+
+  // check if session is still valid
+  $rowQueryResult = $queryResult->fetch_assoc();
+  $exp = strtotime($rowQueryResult['exp']);
+  $curr = time();
+
+  if ($curr > $exp) {
+    return -1;
+  }
+
+  // return user id and user has logged in
+  return $rowQueryResult['userId'];
+}
+
+// check if user is admin
+function isAdmin($con, $userId) {
+  // get the user id
+  $userId = isUserAlreadyLoggedIn($con);
+
+  if ($userId == -1) {
+    return false;
+  }
+
+  // getting the data
+  $sqlQuery = $con->query('SELECT `isAdmin` FROM `USERS` WHERE `user_id` = ' . $userId);
+  $rowQueryResult = $sqlQuery->fetch_assoc();
+
+  // checking
+  if (!$rowQueryResult) {
+    return false;
+  }
+
+  return $rowQueryResult['isAdmin'] == 1;
 }
