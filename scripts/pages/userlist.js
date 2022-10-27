4b56f6fe00bb88c63558708b10449033eff7bb60
irsyadazka <16520096@mahasiswa.itb.ac.id>
2022-10-27 19:20:26 +0700

    feat: styling fe

--- scripts/pages/userlist.js
+++ scripts/pages/userlist.js
@@ -3,13 +3,22 @@ const userLayout = (page) => {
         getAPI(`./api/user/getallusers.php?page=${page}`, (data) => {
             const jsonData = JSON.parse(data);
             users = jsonData.payload;
-            str = `<div class="page-title">Users
+            str = `<div class="page-title">Users of Sepotipayi
                 </div>
-                <div id="users" class="song-list-container">`;
+                <div class="info">
+                    <div class="info-column">Username</div>
+                    <div class="info-column">Email</div>
+                </div>
+                <div id="users" class="user-list-container">`;
 
             str += users.map(user =>
-                `<div class="song-list">
-                    <label>${user.username} - ${user.email}</label>
+                `<div class="user-list">
+                    <div class="user">
+                        <div class="text-info">${user.username}</div>
+                    </div>
+                    <div class="user">
+                        <div class="text-info">${user.email}</div>
+                    </div>
                 </div>`
             ).join("");
 
