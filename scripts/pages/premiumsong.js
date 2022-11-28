9a70592d3c2d2e1559019548f82ed9972696faad
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2022-11-29 00:52:48 +0700

    feat: premium songs player

--- scripts/pages/premiumsong.js
+++ scripts/pages/premiumsong.js
@@ -13,11 +13,12 @@ const getPremiumSong = () => {
     for (let i = 0; i < songs.length; i++){
       const audio = new Audio(songs[i].audio_path);
       audio.onloadedmetadata = function() {
-        const audio_duration = audio.duration;
-        console.log(audio_duration);
-        const str = `<div class="song-list" data-value="${songs[i].song_id}"
+        const audio_duration = parseInt(audio.duration);
+        const str = `<div class="song-list play-song-button" data-value="${songs[i].song_id}"
+                  data-image="./assets/image/song.png"
                   data-audio="${songs[i].audio_path}"
                   data-judul="${songs[i].judul}"
+                  data-penyanyi="${penyanyi}"
                   data-penyanyi-id="${songs[i].penyanyi_id}"
                   data-duration="${audio_duration}"
                 >
@@ -29,6 +30,9 @@ const getPremiumSong = () => {
                     </div>
                   </div>
                   <label class="label">${(audio_duration/60) >> 0}:${("0" + audio_duration%60).slice(-2)}</label>
+                  <img
+                    src="./assets/image/play-btn-black.png" 
+                  alt=""/>
                 </div>`;
         document.getElementById("songs").insertAdjacentHTML("beforeend", str);
       };
